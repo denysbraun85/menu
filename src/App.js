cf7f5c60c6b7feccd878045faca705711f3339f1
Denys <denysbraun1985@gmail.com>
2024-03-18 11:42:09 +0200

    replace carousel code closer to App cjmponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Carousel from "./components/Carousel";
 import React from "react";
+import WeekMenu from "./week_menu";
+
+const date = new Date();
+const formattedDate = date.toLocaleDateString();
 
 function App() {
-  return (
-    <div className="App">
-        <Carousel />
-    </div>
-  );
+  for (const key in WeekMenu) {
+    if (formattedDate === key) {
+      return (
+          <Carousel dataKey={key} dataWeekMenu={WeekMenu}/>
+      );
+    }
+  }
 }
 
 export default App;
--- src/components/Breakfast/index.jsx
+++ src/components/Breakfast/index.jsx
@@ -1,55 +1,46 @@
 import React from "react";
 import leftImg from '../../img/left-side-img/1-left-min.jpg';
-import WeekMenu from "../../week_menu";
 
-const Breakfast = () => {
-
-    const date = new Date();
-    const formattedDate = date.toLocaleDateString();
-
-    for (const key in WeekMenu) {
-        if (formattedDate === key) {
-            return (
-                <div className="menu__slide" data-meal="breakfast" data-side="left">
-                    <img className="menu__img" src={leftImg} alt="1-left-min"/>
-                    <div className="menu__description">
-                        <div className="menu__header-block">
-                            <div className="menu__diet">{WeekMenu[key].diet}</div>
-                            <div className="menu__date">
-                                <span className="menu__date-day">{WeekMenu[key].day}</span>
-                                <span className="menu__date-data">{key}</span>
-                            </div>
-                        </div>
-                        <div className="menu__title">{WeekMenu[key].breakfast.name}</div>
-                        <ul className="menu__meals-block">
-                            <li className="menu__meals-item">
-                                <p className="menu__meals-name">{WeekMenu[key].breakfast.dish_1.name}</p>
-                                <span className="menu__meals-output">{WeekMenu[key].breakfast.dish_1.output}</span>
-                            </li>
-                            <li className="menu__meals-item">
-                                <p className="menu__meals-name">{WeekMenu[key].breakfast.dish_2.name}</p>
-                                <span className="menu__meals-output">{WeekMenu[key].breakfast.dish_2.output}</span>
-                            </li>
-                            <li className="menu__meals-item">
-                                <p className="menu__meals-name">{WeekMenu[key].breakfast.dish_3.name}</p>
-                                <span className="menu__meals-output">{WeekMenu[key].breakfast.dish_3.output}</span>
-                            </li>
-                        </ul>
-                        <ul className="menu__footer-block">
-                            <li className="menu__responsible-item">
-                                <p className="menu__meals-name">{WeekMenu[key].chief_position}</p>
-                                <span className="menu__meals-output">{WeekMenu[key].chief_name}</span>
-                            </li>
-                            <li className="menu__responsible-item">
-                                <p className="menu__responsible-chief">{WeekMenu[key].cook}</p>
-                                <span className="menu__responsible-cook">{WeekMenu[key].cook_name}</span>
-                            </li>
-                        </ul>
+const Breakfast = (props) => {
+    return (
+        <div className="menu__slide" data-meal="breakfast" data-side="left">
+            <img className="menu__img" src={leftImg} alt="1-left-min"/>
+            <div className="menu__description">
+                <div className="menu__header-block">
+                    <div className="menu__diet">{props.data.dataWeekMenu[props.data.dataKey].diet}</div>
+                    <div className="menu__date">
+                        <span className="menu__date-day">{props.data.dataWeekMenu[props.data.dataKey].day}</span>
+                        <span className="menu__date-data">{props.data.dataKey}</span>
                     </div>
                 </div>
-            )
-        }
-    }
+                <div className="menu__title">{props.data.dataWeekMenu[props.data.dataKey].breakfast.name}</div>
+                <ul className="menu__meals-block">
+                    <li className="menu__meals-item">
+                        <p className="menu__meals-name">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_1.name}</p>
+                        <span className="menu__meals-output">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_1.output}</span>
+                    </li>
+                    <li className="menu__meals-item">
+                        <p className="menu__meals-name">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_2.name}</p>
+                        <span className="menu__meals-output">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_2.output}</span>
+                    </li>
+                    <li className="menu__meals-item">
+                        <p className="menu__meals-name">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_3.name}</p>
+                        <span className="menu__meals-output">{props.data.dataWeekMenu[props.data.dataKey].breakfast.dish_3.output}</span>
+                    </li>
+                </ul>
+                <ul className="menu__footer-block">
+                    <li className="menu__responsible-item">
+                        <p className="menu__meals-name">{props.data.dataWeekMenu[props.data.dataKey].chief_position}</p>
+                        <span className="menu__meals-output">{props.data.dataWeekMenu[props.data.dataKey].chief_name}</span>
+                    </li>
+                    <li className="menu__responsible-item">
+                        <p className="menu__responsible-chief">{props.data.dataWeekMenu[props.data.dataKey].cook}</p>
+                        <span className="menu__responsible-cook">{props.data.dataWeekMenu[props.data.dataKey].cook_name}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Breakfast;
